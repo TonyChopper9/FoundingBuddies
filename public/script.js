37d283d2c33bf83399354382664319445b046bf6
NiggoP <43621370+NiggoP@users.noreply.github.com>
2019-08-19 21:04:23 +0200

    add login functions

--- public/script.js
+++ public/script.js
@@ -40,3 +40,13 @@ function initFirebaseAuth() {
   // Listen to auth state changes.
   firebase.auth().onAuthStateChanged(authStateObserver);
 }
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
