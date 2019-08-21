0e70161dce1e065e2ef2512f040009162882b629
TonyChopper9 <43618813+TonyChopper9@users.noreply.github.com>
2019-08-21 21:29:48 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -105,7 +105,7 @@ function addDocument(docId, visibility, number) {
 
 function nextPage() {
 
-  firebase.firestore().collection("users").doc("JdoPIPtw749ypjnmC0am").set({
+  firebase.firestore().collection("users").doc("JdoPIotw749ypjnmC0am").set({
     Username: "testusername",
     mail: "email"
   }).catch(function (error){console.log(error)});
