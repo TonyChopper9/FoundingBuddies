1fa282e860ec0f8847c748ff0ab1b34e2cb4ff0f
NiggoP <43621370+NiggoP@users.noreply.github.com>
2019-08-25 00:16:37 +0200

    a

--- public/login.js
+++ public/login.js
@@ -19,11 +19,6 @@ function signUp(){
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((user) => {
-    user.sendEmailVerification().then(function() {
-      // Email sent.
-    }).catch(function(error) {
-      // An error happened.
-    });
     firestore.collection("users").doc(user.user.uid).set({
         Username: username,
         mail: email,
