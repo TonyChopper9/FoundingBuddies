07794304db22ca589372605961c972721add478d
TonyChopper9 <43618813+TonyChopper9@users.noreply.github.com>
2019-08-26 22:59:28 +0200

    Update login.js

--- public/login.js
+++ public/login.js
@@ -81,7 +81,10 @@ function signInWithEmail() {
 function signInWithGoogle() {
     // Sign into Firebase using popup auth & Google as the identity provider.
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function () {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var user = result.user;
+        console.log(user.displayName);
+        console.log(user.uid);
         flag1 = true;
         flag2 = true;
         redirectHome()
