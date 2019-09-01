0cd45733d6a4a10f326e3eca393711dadb432401
TonyChopper9 <43618813+TonyChopper9@users.noreply.github.com>
2019-09-01 23:59:50 +0200

    Update notifications.js

--- public/notifications.js
+++ public/notifications.js
@@ -153,6 +153,7 @@ function authStateObserver(user) {
             console.log(x);
         }) ;
         console.log(user.providerData.entries());
+        console.log(user.providerData.entries().next().value);
         console.log(user.providerData.entries()[0]);
         if (user.providerId == "google.com") {
             changeEmailBtn.style.display = "none";
