f9d7bbc5f0811b105701b51814c87ab8e9d990f3
TonyChopper9 <43618813+TonyChopper9@users.noreply.github.com>
2019-09-02 23:02:37 +0200

    Update notifications.js

--- public/notifications.js
+++ public/notifications.js
@@ -152,8 +152,7 @@ function authStateObserver(user) {
             userMailElement.innerHTML = userMail + "<br>(not verified yet)";
             addSendEmailVerifyButton();
         }
-        console.log(user.providerData.entries().next().value[1].providerId + "<-- supplier");
-        if (user.providerData.entries().next().value[1] == "google.com") {
+        if (user.providerData.entries().next().value[1].providerId == "google.com") {
             changeEmailBtn.style.display = "none";
             resetPasswordBtn.style.display = "none";
         }
