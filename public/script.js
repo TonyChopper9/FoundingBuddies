1ff517eedc1e1f13fd12d77ffa9fe043494b1c87
NiggoP <43621370+NiggoP@users.noreply.github.com>
2019-10-07 00:47:00 +0200

    ure

--- public/script.js
+++ public/script.js
@@ -408,6 +408,21 @@ function deletePost(docIdNo) {
     });
 }
 
+function search() {searchInput
+  const term = document.getElementById("searchInput").value;
+  firestore.collection("posts").where("header", "==", term)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+}
+
 //Shortcuts to Document Elements
 //var userPicElement = document.getElementById('user-pic');
 
