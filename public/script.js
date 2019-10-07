5b05e95407e1ca432816e107eeaa9a4dc13b6684
NiggoP <43621370+NiggoP@users.noreply.github.com>
2019-10-07 15:41:09 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -32,7 +32,7 @@ function loadPosts(keyword = "", filter = "") {
   }
   else {
     //load only docs with filter
-    firestore.collection("posts").where("uni", "==", filter).orderBy("Date", "desc").get()..then(function(querySnapshot) {
+    firestore.collection("posts").where("uni", "==", filter).orderBy("Date", "desc").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             //console.log(doc.id, " => ", doc.data());
